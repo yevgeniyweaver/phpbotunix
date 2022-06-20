baddca53096d6f285eee5589b5ae5b092f8b9642
yevgeniyhyperman <risetobase@gmail.com>
2022-06-20 21:46:21 +0300

    #master: add api small config

--- routes/api.php
+++ routes/api.php
@@ -14,8 +14,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+
+Route::group([
+//    'namespace' => 'API\V1',
+    'prefix' => 'user',
+    'middleware' => 'auth_api', //auth:api
+], function () {
+    Route::get('{id}', function ($id) {
+        $user = \App\Models\User::find($id);
+        if (!$user) {
+            return response('wrong', 404);
+        }
+
+        return $user;
+    });
+    //Route::get('show/{id}', 'SspController@show')->middleware('role:dev');
 });
 
 
